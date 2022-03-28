5fe88c68477d2d98f90263329039b50201fcb2f9
SanjeevCA <12109@coderacademy.edu.au>
2022-03-28 14:54:44 +1000

    Cafe app complete

--- TERM1/WEEK7/DAY1/cafe.rb
+++ TERM1/WEEK7/DAY1/cafe.rb
@@ -20,6 +20,12 @@ def menuOptions
     puts
 end
 
+def printMenu
+    $array_products.each_with_index do |item, i|
+        puts "      #{i + 1}.   #{item}"
+    end
+end
+
 def showMenu
     system 'clear'
     puts "=========================================="
@@ -27,9 +33,7 @@ def showMenu
     puts "Here is our menu."
     puts "=========================================="
     puts
-    $array_products.each_with_index do |item, i|
-        puts "      #{i + 1}.   #{item}"
-    end
+    printMenu
 end
 
 
@@ -54,7 +58,17 @@ def addProduct
     puts "=========================================="
     puts
     print "Item to ADD: "
-    $array_products.push(gets)
+    product = gets.chomp
+    if $array_products.include?(product)
+        puts
+        puts "Item already exists on the menu."
+    else
+        $array_products.push(product)
+        puts
+        puts "#{product} has been added to the menu!"
+    end
+
+
 end
 
 def editProduct
@@ -62,6 +76,24 @@ def editProduct
     puts "=========================================="
     puts "Edit the product details"
     puts "=========================================="
+    puts
+
+    printMenu
+    puts
+    print "Which menu number would you like to edit? "
+
+    editIndex = gets.chomp.to_i - 1;
+
+    puts
+    puts "      #{editIndex + 1}.   #{$array_products[editIndex]}"
+    puts
+    print "Update this to: "    
+
+    $array_products[editIndex] = gets.chomp
+    
+    puts
+    puts "Menu item at #{editIndex + 1}. is now #{$array_products[editIndex]}"
+    
 end
 
 def removeProduct
@@ -70,9 +102,28 @@ def removeProduct
     puts "Remove a specfic product from the menu"
     puts "=========================================="
     puts
+    printMenu
+    puts
     print "Item to REMOVE: "
     item = gets.chomp
-    $array_products.delete(item)
+
+    if $array_products.include?(item)
+        puts
+        puts "Are you sure you wish to delete #{item}? (y/n)"
+        confirm = gets.chomp
+        if confirm == "y"
+            puts
+            puts "#{item} has been deleted."
+            $array_products.delete(item)
+        else
+            puts
+            puts "Deletion aborted..."
+        end
+    else
+        puts
+        puts "#{item} does not exist on the menu."
+    end
+
 end
 
 while $option != "5"
